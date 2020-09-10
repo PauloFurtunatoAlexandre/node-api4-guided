27bde40700c7627d434bda98c954bf87c218b548
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-09 21:41:34 -0500

    created start script in dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 4000;
+
+// console.log(process.env);
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
